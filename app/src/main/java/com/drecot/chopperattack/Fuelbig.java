92f1a3a73bee7d5b4b3fb201cfda7074708e788c
Drecot <kennedyizuegbu@yahoo.com>
2017-04-18 11:10:26 +0100

    april 18 2017

---
+++ app/src/main/java/com/drecot/chopperattack/Fuelbig.java
@@ -0,0 +1,72 @@
+package com.drecot.chopperattack;
+
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+
+import java.util.Random;
+
+public class Fuelbig extends GameObject {
+
+    private Bitmap spritesheet;
+    private long startTime;
+    private Random random = new Random();
+    private Animation animation = new Animation();
+    private int maxBorderHeight = 856;
+
+
+    public Fuelbig(Bitmap res, int w, int h, int numFrames) {
+        x = GamePanel.WIDTH + 20000;
+        y = random.nextInt(GamePanel.HEIGHT-20);
+        dy =0;
+        dx = +GamePanel.MOVESPEED;
+        height = h;
+        width = w;
+
+
+        Bitmap[] image = new Bitmap[numFrames];
+        spritesheet = res;
+
+        for (int i = 0; i < image.length; i++)
+        {
+            image[i] = Bitmap.createBitmap(spritesheet, 0, i*height, width, height);
+        }
+        animation.setFrames(image);
+        animation.setDelay(100-dx);
+        animation.update();
+    }
+    public void update()
+    {
+        if (x < 0) {
+            reset();
+        }
+        x += dx;
+        dx = dx- 1;
+
+        if (dx <= -15) {
+            dx = -15;
+        }
+
+        animation.update();
+    }
+    public void draw(Canvas canvas)
+    {
+        try {
+            canvas.drawBitmap(animation.getImage(),x,y,null);
+        }catch (Exception e){}
+    }
+    public void reset(){
+        x = GamePanel.WIDTH + 20000 ;
+        y = random.nextInt(GamePanel.HEIGHT-20);
+        dy = 0;
+        dx = +GamePanel.MOVESPEED;
+    }
+
+    public void fuelCollectedBig(){
+
+        reset();
+    }
+
+
+}
+
